06c578521c02be29d114f0225cc18ba2138e6e1f
Shivanshjugran07 <shivanshjugran7@gmail.com>
2026-02-07 09:48:46 +0530

    Set up React app structure

---
+++ frontend/App.js
@@ -0,0 +1,15 @@
+// React App Entry Point
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Customer Churn Prediction</h1>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
